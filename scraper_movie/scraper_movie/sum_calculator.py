a26c04d43a469da457f42cb6bd1d91a2b18bf9b3
s1lver29 <nikitaselver@yandex.ru>
2026-01-30 17:52:25 +0300

    Implemented SumCalculator class to perform summation of numeric data

---
+++ scraper_movie/scraper_movie/sum_calculator.py
@@ -0,0 +1,32 @@
+from typing import Union, Tuple
+
+class SumCalculator:
+    def __init__(self, *numbers: Union[int, float]) -> None:
+        self.numbers = []
+        if numbers:
+            self.add_numbers(numbers)
+
+    def add_numbers(self, numbers: Tuple[Union[int, float], ...]) -> None:
+        for number in numbers:
+            self.add_number(number)
+
+    def calculate_sum(self) -> Union[int, float]:
+        return sum(self.numbers) if self.numbers else 0
+
+    def add_number(self, number: Union[int, float]) -> None:
+        if not isinstance(number, (int, float)):
+            raise TypeError(f"Expected int or float, got {type(number).__name__}")
+        self.numbers.append(number)
+
+    def clear(self) -> None:
+        self.numbers = []
+
+    @property
+    def numbers(self) -> Tuple[Union[int, float], ...]:
+        return tuple(self._numbers)
+
+    def __str__(self) -> str:
+        return f'SumCalculator({self.numbers})'
+
+    def __repr__(self) -> str:
+        return f'SumCalculator(numbers={self.numbers})'
